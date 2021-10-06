f6958e65bfdb0a418e8a662949f383b34b77aeb7
Joel <thatjoelallison@gmail.com>
2021-10-06 16:33:10 +0100

    realised that you don't need suits in blackjack d(^_^)

--- src/com/joelallison/Main.java
+++ src/com/joelallison/Main.java
@@ -4,28 +4,15 @@ import java.util.*;
 
 public class Main {
 
-    static String format(String input){
-        String suit = new String();
-        switch(input.charAt(0)){
-            case 'c':
-                suit = "Clubs";
-                break;
-            case 'd':
-                suit = "Diamonds";
-                break;
-            case 'h':
-                suit = "Hearts";
-                break;
-            case 's':
-                suit = "Spades";
-                break;
-            default:
-                suit = ":///";
-                break;
-        }
+    static void printHand(ArrayList<Character> hand){
+        for (int i = 0; i < hand.size()-1; i++) {
+            System.out.print(format(hand.get(i)) + ", ");
+        }System.out.print(format(hand.get(hand.size()-1)) + ".");
+    }
 
+    static String format(char input){
         String value = "";
-        switch(input.charAt(1)){
+        switch(input){
             case '1':
                 value = "10";
                 break;
@@ -42,10 +29,10 @@ public class Main {
                 value = "Ace";
                 break;
             default:
-                value = String.valueOf(input.charAt(2));
+                value = String.valueOf(input);
         }
 
-        return (value + " of " + suit);
+        return (value);
     }
 
     public static void main(String[] args) {
@@ -53,36 +40,35 @@ public class Main {
         Scanner userInput = new Scanner(System.in);
 
         //generate cards
-        final char[] suits = {'c','d','h','s'};
-        final String[] values = {"A","2","3","4","5","6","7","8","9","1","J","Q","K"};
-        ArrayList<String> cards = new ArrayList<String>();
+        final char[] values = {'A','2','3','4','5','6','7','8','9','1','J','Q','K'};
+        ArrayList<Character> cards = new ArrayList<Character>();
         for (int suit = 0; suit < 4; suit++) {
             for (int i = 0; i < 13; i++) {
-                cards.add(suits[suit] + values[i]);
+                cards.add(values[i]);
             }
         }
 
-        ArrayList<String> playerHand = new ArrayList<String>();
-        ArrayList<String> computerHand = new ArrayList<String>();
+        ArrayList<Character> playerHand = new ArrayList<Character>();
+        ArrayList<Character> computerHand = new ArrayList<Character>();
 
         System.out.println("Blackjack");
 
         int count = 0;
         //game loop
-        boolean playing = false;
+        boolean playing = true;
         do{
             count++;
             int dealtCard = random.nextInt(cards.size());
-            if(count % 2 == 1){
+            if(count % 2 == 0){
                 //player turn
                 System.out.println("Round " + count/2);
                 playerHand.add(cards.get(dealtCard));
 
                 System.out.println(cards.get(dealtCard));
 
-                for (String card:playerHand){
-                    System.out.println(format(card));
-                }
+                System.out.println(playerHand);
+
+                printHand(playerHand);
             }else{
                 //computer "turn"
                 computerHand.add(cards.get(dealtCard));
